7c674fde3fb423f80f15ca2a743a931f33035bf7
Rusica Alexantru <aalexandruslan@gmail.com>
2021-02-19 23:04:19 +0200

    Exercise with arrays
    Also open the project from another IDE.

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,1 +1,25 @@
 //acasa lucru cu funct de la array
+//--------------------------
+//-
+let arr = [];
+
+alert(arr);
+/*
+let arr1 = ["Car", "Space", "Ball"];
+let arr2 = ["apple", "orange"];
+arr1 = arr2;
+arr1.push("Bannana")
+alert(arr1);
+*/
+
+/*for (let i = 0; i < 10; i++){ // == arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    arr.push(arr.length+1);
+}*/
+
+/*
+arr.shift(); //take first
+arr.pop(); //take last
+
+arr.unshift(FlyLy); //add to the begining
+arr.push(FlyLy); // add to the end
+*/
