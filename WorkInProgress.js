0368de822428325a6175b3d0cb449c283ff89b9d
Rusica Alexantru <aalexandruslan@gmail.com>
2021-02-22 19:13:26 +0200

    splice, slice
    Practice

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,1 +1,14 @@
 //acasa lucru cu funct de la array
+let arr = [1,2,3,4,5,6,7,8,9];
+//----splice/slice
+//arr.splice(3,2);
+//arr.splice(0, 1, "Space", "fly");
+//let removed = arr.splice(0, 2);//removed contains deleted elemets
+//arr.slice(3, 8) // shows only (n1+1 -> n2) where : arr.slice(n1, n2)
+//arr.slice(-3) //shows last 3 elements
+//----
+/*
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  alert(`${item} is at index ${index} in ${array}`);
+});
+*/
