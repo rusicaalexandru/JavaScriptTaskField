f4d98877750155af0c1fba9ee3d87806529a1cea
Rusica.Alexandru <aalexandruslan@gmail.com>
2021-02-25 13:50:06 +0200

    Backup

--- WorkColection.js
+++ WorkColection.js
@@ -177,3 +177,45 @@ arr.push(FlyLy); // add to the end
         break;
     }
 }*/
+//----------------------------------------------------------------------------- Array (remember)
+
+//acasa lucru cu funct de la array
+
+//----splice/slice
+//arr.splice(3,2);
+//arr.splice(0, 1, "Space", "fly");
+//let removed = arr.splice(0, 2);//removed contains deleted elemets
+//arr.slice(3, 8) // shows only (n1+1 -> n2) where : arr.slice(n1, n2)
+//arr.slice(-3) //shows last 3 elements
+//----
+/*
+["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
+  alert(`${item} is at index ${index} in ${array}`);
+});
+*/
+/*
+//--------
+const users = [
+  {id: 1, name: "Fly"},
+  {id: 2, name: "Space"},
+  {id: 3, name: "Eren"}
+];
+
+let user = users.find(item => item.id == 3);
+
+alert(user.name); // Eren
+*/
+/*
+//--------
+let users = [
+    { id: 1, name: "Johny" },
+    { id: 2, name: "Peter" },
+    { id: 3, name: "Harly" }
+];
+
+// returns array of the first two users
+let someUsers = users.filter(item => item.id < 3);
+
+alert(someUsers.length); // 2
+*/
+//-----------------------------------------------------------------------------Tema de acasa
--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,40 +1,69 @@
-//acasa lucru cu funct de la array
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//-------------------------------------------------------------------------------------------------Ex3.1
+// const howManyNumbers = prompt("How many numbers you will insert?");
+// let arr = [];
+// for (let i = 0; i < howManyNumbers; i++) { //Add numbers to array arr
+//     arr.push(prompt(`Insert number ${i+1}`)); //+1 to start counting at 1 instead of 0
+// }
+//output for :
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+//output while :
+// let i = 0;
+// while (i != arr.length) {
+//     console.log(arr[i]);
+//     i++;
+// }
+//output do While :
+// let i = 0;
+// do {
+//     console.log(arr[i]);
+//     i++
+// } while (i != arr.length)
+//output for in :
+// for (let element in arr) {
+//     console.log(arr[element]);
+// }
+//output for of :
+// for (let element of arr) {
+//     console.log(element);
+// }
+//output ForEach :
+//arr.forEach(element => console.log(element));
+//output Map :
+//const mapArr = arr.map(elem => console.log(elem));
+//-------------------------------------------------------------------------------------------------Ex3.2
+let students = [];
+//const studentCount = prompt("How many students you will insert?");
 
-//----splice/slice
-//arr.splice(3,2);
-//arr.splice(0, 1, "Space", "fly");
-//let removed = arr.splice(0, 2);//removed contains deleted elemets
-//arr.slice(3, 8) // shows only (n1+1 -> n2) where : arr.slice(n1, n2)
-//arr.slice(-3) //shows last 3 elements
-//----
-/*
-["Bilbo", "Gandalf", "Nazgul"].forEach((item, index, array) => {
-  alert(`${item} is at index ${index} in ${array}`);
+// for (let i = 0; i < studentCount; i++) {
+//     const studentName = prompt("Introduce student name!");
+//     const gradeCount = prompt("How many grades student have?");
+//     const studentGrades = [];
+//     for (let o = 0; o < gradeCount; o++) { // add grades
+//         studentGrades.push(+prompt("enter the grade"));
+//     }
+//     const medie = studentGrades.reduce((acc, val) => acc + val) / studentGrades.length; // calculate the sum of the grades and divide it by gradelds lenght
+//     students.push({ Name: studentName, Medie: medie, Grades: studentGrades }); //push information to the array(students)
+
+// }
+//-----------
+students.push({ Name: "Dorin", Medie: 6, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+students.push({ Name: "Marin", Medie: 3, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+students.push({ Name: "Peter", Medie: 4, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+students.push({ Name: "Sort", Medie: 8, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+
+students.map(stud => { //show name and grades
+    console.log("Name :" + stud.Name + "\nNota medie :" + stud.Medie + "\nNotele :" + stud.Grades);
 });
-*/
-/*
-//--------
-const users = [
-  {id: 1, name: "Fly"},
-  {id: 2, name: "Space"},
-  {id: 3, name: "Eren"}
-];
+console.log("----------------------------------------------------------");
+students.map(stud => { // log student with grade <= 5
+    if (stud.Medie <= 5) {
+        console.log("Studentul cu nota mai mica sau egala cu 5 : \n" + stud.Name + " cu media : " + stud.Medie);
+    }
+});
+console.log("----------------------------------------------------------");
 
-let user = users.find(item => item.id == 3);
-
-alert(user.name); // Eren
-*/
-/*
-//--------
-let users = [
-    { id: 1, name: "Johny" },
-    { id: 2, name: "Peter" },
-    { id: 3, name: "Harly" }
-];
-
-// returns array of the first two users
-let someUsers = users.filter(item => item.id < 3);
-
-alert(someUsers.length); // 2
-*/
+students.sort((a, b) => a.Medie - b.Medie); // biggest grade and the lowest
+console.log(`Cea mai mica Medie si cea mai mare Medie : \n ${students[0].Medie} ${students[0].Name} \n ${students[students.length - 1].Medie} ${students[students.length - 1].Name}`);
+console.log(students.reverse()); // Descendent by Medie
