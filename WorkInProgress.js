69b67f3126a207751a4a1bfda5969958a66ef215
Rusica Alexantru <aalexandruslan@gmail.com>
2021-02-26 17:00:31 +0200

    Backup

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -38,14 +38,28 @@
 //const studentCount = prompt("How many students you will insert?");
 
 //-------------------------------------------------------------------------------------------------Ex3.8
-const colors = ["red", "green", "blue"];
-const checkColor = (color) => {
-    for (const col of colors){
-        if (color == col){
-            return colors.indexOf(color);
-        }
-    }
-    return -1
-}
-console.log(checkColor("red"));
+// const colors = ["red", "green", "blue"];
+// const checkColor = (color) => {
+//     for (const col of colors){
+//         if (color == col){
+//             return colors.indexOf(color);
+//         }
+//     }
+//     return -1
+// }
+// console.log(checkColor("red"));
 //-------------------------------------------------------------------------------------------------Ex3.9
+// const arr = [1, "a", 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
+// const filter = (array) => array.filter(Number);
+//  console.log(filter(arr));
+//-------------------------------------------------------------------------------------------------Ex3.10
+const arr = [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
+const uniqueSorted = (array) => { //js / custome
+    //js
+    //return array.filter((v, i, a) => a.indexOf(v) === i);
+    //custume
+    array.sort((a, b) => a - b);
+    return
+}
+//arr.filter((v, i, a) => a.indexOf(v) === i)
+console.log(uniqueSorted(arr));
