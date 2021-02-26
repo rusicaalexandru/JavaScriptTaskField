d6864b62787040ac3c1a727c7c5ec0620ea8b6eb
Rusica Alexantru <aalexandruslan@gmail.com>
2021-02-26 19:56:34 +0200

    HomeWork tasks
    Ex3.8 - Ex3.10

--- WorkColection.js
+++ WorkColection.js
@@ -318,3 +318,24 @@ alert(someUsers.length); // 2
 //     return array.slice(numStart-1, numEnd);
 // }
 // console.log(showArray(2,4, arr));
+//-------------------------------------------------------------------------------------------------Ex3.8
+// const colors = ["red", "green", "blue"];
+// const checkColor = (color) => {
+//     for (const col of colors){
+//         if (color == col){
+//             return colors.indexOf(color);
+//         }
+//     }
+//     return -1
+// }
+// console.log(checkColor("red"));
+//-------------------------------------------------------------------------------------------------Ex3.9
+// const arr = [1, "a", 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
+// const filter = (array) => array.filter(Number);
+//  console.log(filter(arr));
+//-------------------------------------------------------------------------------------------------Ex3.10
+// const arr = [8, 7,  2,  2,  3, 4, 2,  5, 5, 6, 7, 8, 5, 9, 0, 1];
+// const uniqueSorted = (array) => array.filter((v, i, a) => a.indexOf(v) === i);
+// arr.sort((a,b) => a-b)
+// console.log(uniqueSorted(arr));
+//-------------------------------------------------------------------------------------------------Ex6
