f268edc88da99bd76e3d71647f10dfcc885f1586
Rusica.Alexandru <aalexandruslan@gmail.com>
2021-02-26 10:25:41 +0200

    Backup

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,3 +1,4 @@
+//Acasa -> obiec javascript, tema 4.1 ..
 //-------------------------------------------------------------------------------------------------Ex3.1
 // const howManyNumbers = prompt("How many numbers you will insert?");
 // let arr = [];
@@ -33,7 +34,7 @@
 //output Map :
 //const mapArr = arr.map(elem => console.log(elem));
 //-------------------------------------------------------------------------------------------------Ex3.2
-let students = [];
+//let students = [];
 //const studentCount = prompt("How many students you will insert?");
 
 // for (let i = 0; i < studentCount; i++) {
@@ -48,22 +49,65 @@ let students = [];
 
 // }
 //-----------
-students.push({ Name: "Dorin", Medie: 6, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
-students.push({ Name: "Marin", Medie: 3, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
-students.push({ Name: "Peter", Medie: 4, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
-students.push({ Name: "Sort", Medie: 8, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+// students.push({ Name: "Dorin", Medie: 6, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+// students.push({ Name: "Marin", Medie: 3, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+// students.push({ Name: "Peter", Medie: 4, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
+// students.push({ Name: "Sort", Medie: 8, Grades: [3, 4, 5, 7, 8] }); //push information to the array(students)
 
-students.map(stud => { //show name and grades
-    console.log("Name :" + stud.Name + "\nNota medie :" + stud.Medie + "\nNotele :" + stud.Grades);
-});
-console.log("----------------------------------------------------------");
-students.map(stud => { // log student with grade <= 5
-    if (stud.Medie <= 5) {
-        console.log("Studentul cu nota mai mica sau egala cu 5 : \n" + stud.Name + " cu media : " + stud.Medie);
-    }
-});
-console.log("----------------------------------------------------------");
+// students.map(stud => { //show name and grades
+//     console.log("Name :" + stud.Name + "\nNota medie :" + stud.Medie + "\nNotele :" + stud.Grades);
+// });
+// console.log("----------------------------------------------------------");
+// students.map(stud => { // log student with grade <= 5
+//     if (stud.Medie <= 5) {
+//         console.log("Studentul cu nota mai mica sau egala cu 5 : \n" + stud.Name + " cu media : " + stud.Medie);
+//     }
+// });
+// console.log("----------------------------------------------------------");
 
-students.sort((a, b) => a.Medie - b.Medie); // biggest grade and the lowest
-console.log(`Cea mai mica Medie si cea mai mare Medie : \n ${students[0].Medie} ${students[0].Name} \n ${students[students.length - 1].Medie} ${students[students.length - 1].Name}`);
-console.log(students.reverse()); // Descendent by Medie
+// students.sort((a, b) => a.Medie - b.Medie); // biggest grade and the lowest
+// console.log(`Cea mai mica Medie si cea mai mare Medie : \n ${students[0].Medie} ${students[0].Name} \n ${students[students.length - 1].Medie} ${students[students.length - 1].Name}`);
+// console.log(students.reverse()); // Descendent by Medie
+
+// //Media of an student is bigger than Media of the entier class
+// const medieClass = () => {
+//     let sum = 0;
+//     for (st of students) {
+//         sum += st.Medie;
+//     }
+//     return sum / students.length;
+// }
+// students.map(stud => {
+//     if (stud.Medie <= medieClass()) {
+//         console.log("Studentii cu nota mai mare ca media clasei este " + stud.Name + "Cu media de :" + stud.Medie);
+//     }
+// });
+//-------------------------------------------------------------------------------------------------Ex3.3
+// let styles = ["Джаз", "Блюз"];
+// console.log(styles);
+// styles.push("Рок-н-ролл");
+// console.log(styles);
+// styles.splice(styles.length / 2, 1, "Классика");
+// console.log(styles);
+// styles.shift(1);
+// console.log(styles);
+// styles.splice(0, 0, "Рэп", "Регги")
+// console.log(styles);
+//-------------------------------------------------------------------------------------------------Ex3.4 Unfinished
+// let arr = [];
+// const sumInputNumbers = () => {
+//     let sum = 0;
+//     while (true) {
+//         const input = (+prompt("Entera number :"));
+//         if (input == "null" || input == null || input == "") {
+//             return sum / arr.length;
+//             break;
+//         }
+//         arr.push(input);
+//         sum += input;
+//     }
+// }
+
+// console.log(arr);
+// console.log(sumInputNumbers());
+//-------------------------------------------------------------------------------------------------Ex3.5
