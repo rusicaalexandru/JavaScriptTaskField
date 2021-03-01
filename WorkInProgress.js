8276a75e678ff70ec9acd11a85f90aaf2fb8e573
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-01 10:11:12 +0200

    backup

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,49 +1,25 @@
 //Acasa -> obiec javascript, tema 4.1 ..
-//-------------------------------------------------------------------------------------------------Ex3.1
-// const howManyNumbers = prompt("How many numbers you will insert?");
-// let arr = [];
-// for (let i = 0; i < howManyNumbers; i++) { //Add numbers to array arr
-//     arr.push(prompt(`Insert number ${i+1}`)); //+1 to start counting at 1 instead of 0
-// }
-//output for :
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-//output while :
-// let i = 0;
-// while (i != arr.length) {
-//     console.log(arr[i]);
-//     i++;
-// }
-//output do While :
-// let i = 0;
-// do {
-//     console.log(arr[i]);
-//     i++
-// } while (i != arr.length)
-//output for in :
-// for (let element in arr) {
-//     console.log(arr[element]);
-// }
-//output for of :
-// for (let element of arr) {
-//     console.log(element);
-// }
-//output ForEach :
-//arr.forEach(element => console.log(element));
-//output Map :
-//const mapArr = arr.map(elem => console.log(elem));
-//-------------------------------------------------------------------------------------------------Ex3.2
-//let students = [];
-//const studentCount = prompt("How many students you will insert?");
+
 
 //-------------------------------------------------------------------------------------------------Ex6.1
-const Persone = (name, surname) => {
-    return {
-        name, // same as name: name
-        surname,
-    };
-}
-const pers = Persone("Fred ", "Lazofsky");
-console.log(pers);
+const user = {};
+user.name = "Jhon";
+user.surName = "Smith";
+user.name = "Pete";
+delete user.name;
+console.log(user.name)
 //-------------------------------------------------------------------------------------------------Ex6.2
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const calculate = () => {
+    let sum = 0;
+    for (a of Object.values(salaries)){
+        sum += a;
+    }
+    return sum;
+}
+console.log(calculate());
