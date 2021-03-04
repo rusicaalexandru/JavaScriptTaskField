03137888d8f386c28143c3749472f4951ffe072b
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-04 14:40:22 +0200

    Ex6.1
    complete missed last 2 rows

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,11 +1,15 @@
 //Tema pentru acasa 04.03.2021
 //----------------------------------------------------------Ex6
 //----------------------------------------------------------Ex6.1
-class Persone {
-    constructor(name, surName) {
-        this._name = name;
-        this._surName = surName;
-    }
-}
-const person = new Persone("Ivan", "Ivanov");
-console.log(person);
+// class Persone {
+//     constructor(name, surName) {
+//         this.name = name;
+//         this.surName = surName;
+//     }
+// }
+// const person = new Persone("Ivan", "Ivanov");
+// console.log(person);
+//
+// console.log(person.name);
+// console.log(person.surName);
+//----------------------------------------------------------Ex6.2
