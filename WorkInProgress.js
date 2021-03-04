6d698114f1101d894c33fd360ebbbe16c445278e
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-04 17:09:27 +0200

    Ex6.4
    Validation with Regex

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -34,34 +34,55 @@
 // console.log(worker.getSurname());
 // console.log(worker.setSurname("Petrov"));
 // console.log(worker.getSurname());
+//----------------------------------------------------------Ex6.4
+class Validator { // Regex
+    static isEmail(str) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
+    }
+    static isDate(str) {
+        return  /(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d/.test(str);
+    }
+    static isPhone(str) {
+        return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(str);
+    }
+    static isDomain(str) {
+        return /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(str);
+    }
+}
+const validator = new Validator();
+
+console.log(Validator.isEmail("phphtml@mail.ru"));
+console.log(Validator.isDate("12.05.2020"));
+console.log(Validator.isPhone("123-456-7890"));
+console.log(Validator.isDomain("google.com"));
 //----------------------------------------------------------Ex6.5
-class User{
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
-
-    }
-    getFullName(){
-        return `${this.name} ${this.surname}`
-    }
-}
-
-class Student extends User{
-    constructor(name, surname, year) {
-        super();
-        this.name = name;
-        this.surname = surname;
-        this.year = year;
-    }
-    getCourse(){
-        return  new Date().getFullYear() - this.year;
-    }
-}
-
-const student = new Student("Ivan", "Ivanov", 2018);
-
-console.log(student.name);
-console.log(student.surname);
-console.log(student.getFullName());
-console.log(student.year);
-console.log(student.getCourse());
+// class User{
+//     constructor(name, surname) {
+//         this.name = name;
+//         this.surname = surname;
+//
+//     }
+//     getFullName(){
+//         return `${this.name} ${this.surname}`
+//     }
+// }
+//
+// class Student extends User{
+//     constructor(name, surname, year) {
+//         super();
+//         this.name = name;
+//         this.surname = surname;
+//         this.year = year;
+//     }
+//     getCourse(){
+//         return  new Date().getFullYear() - this.year;
+//     }
+// }
+//
+// const student = new Student("Ivan", "Ivanov", 2018);
+//
+// console.log(student.name);
+// console.log(student.surname);
+// console.log(student.getFullName());
+// console.log(student.year);
+// console.log(student.getCourse());
