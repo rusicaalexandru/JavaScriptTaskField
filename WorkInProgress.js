f1325f0a36a2b10fe15f255e3e74785ac7119b08
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-04 14:37:53 +0200

    Ex6.1

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -1,1 +1,11 @@
-
+//Tema pentru acasa 04.03.2021
+//----------------------------------------------------------Ex6
+//----------------------------------------------------------Ex6.1
+class Persone {
+    constructor(name, surName) {
+        this._name = name;
+        this._surName = surName;
+    }
+}
+const person = new Persone("Ivan", "Ivanov");
+console.log(person);
