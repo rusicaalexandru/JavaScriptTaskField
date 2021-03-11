6e2319402ba92d711e79a5f61aedd0467d1e48aa
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-11 14:45:10 +0200

    Finished

--- WorkInProgress.js
+++ WorkInProgress.js
@@ -12,20 +12,58 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     //----------------------------------------------------------------------7.4
-
-    for (let i = 1; i <= 20; i++){
-        setTimeout(() => {
+    let iter = 0;
+    setInterval(() => {
+        if (iter <= 20) {
             const node = document.createElement("LI");
-            const textnode = document.createTextNode(`${i}`);
+            const textnode = document.createTextNode(`${iter}`);
             node.appendChild(textnode);
             document.getElementById("olListId").appendChild(node);
-            if(i % 2 === 0) {
+            if(iter % 2 === 0) {
                 node.style.backgroundColor = "green";
             } else {
                 node.style.backgroundColor = "purple";
             }
-        }, 2000)
-    }
+            iter++;
+        }
+        }, 2000);
+    //----------------------------------------------------------------------7.6
+    table();
+    isNum();
 });
 
+//----------------------------------------------------------------------------------------------7.6
+const tableBody = document.querySelector('#tableId');
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8'];
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+const table = () => {
+    letters.forEach((letter, index) => {
+        const tr = document.createElement('tr');
+
+        numbers.forEach((number, i) => {
+            const td = document.createElement('td');
+
+            if ((index + i) % 2) {
+                td.style.backgroundColor = 'darkgrey';
+            }
+
+            td.innerHTML = letter + number;
+            tr.append(td);
+        });
+
+        tableBody.append(tr);
+    });
+};
+
+//----------------------------------------------------------------------------------------------7.15
+
+const input = document.getElementById('input');
+
+const isNum = () => {
+    input.addEventListener('focusout', () => {
+        if (!Number(input.value)) {
+            input.value = '';
+        }
+    });
+};
