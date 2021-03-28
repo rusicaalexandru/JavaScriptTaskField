a2c42a6580c3c5eb99544b332ed6405c13fa8856
Rusica Alexantru <aalexandruslan@gmail.com>
2021-03-28 22:20:24 +0300

    backup

--- Pizza/scripts/index.js
+++ Pizza/scripts/index.js
@@ -10,10 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     });
     //-----------------------------------------------------------------------------
-    price = document.getElementById("final_price").innerText;
-    console.log(price)
+    // let price = document.getElementById("final_price").innerText;
+    // let list = document.addEventListener("change", () => {
+    //     let temp = document.getElementById("options");
+    //     let temp2 = temp.getElementsByTagName("li").namedItem();
+    //     console.log(temp1);
+    //     console.log(temp2);
+    //     //price =
+    // });
+    //console.log(price)
+    //let inputs = document.querySelectorAll('input[name]');
 
+    let price = 0;
+    let priceCash = [];
+    refresh()
+    document.addEventListener("input", () => {
+        console.clear();
+        refresh()
+    });
+    //console.log(price);
 
-    
 });
 
+refresh = () => {
+    document.querySelectorAll("input[name]:checked").forEach(elem => {
+        priceCash.push(elem.value)
+        //price += +elem.value;
+        console.log(elem.value)
+    });
+}
