1fc1c2bc9ad65ac256ba97e1ed6ed9652303ff18
Rusica Alexantru <aalexandruslan@gmail.com>
2021-05-20 17:36:43 +0300

    list of default pizza

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,39 @@
+let pizzaList = [
+    {name:"peperoni",price:80},
+    {name:"pasta",price:85},
+    {name:"capricioasa",price:80},
+    {name:"bento",price:90},
+    {name:"taraneasca",price:100},
+    {name:"dio",price:130},
+    {name:"abdul",price:60},
+    {name:"monako",price:750}
+];
+
 document.addEventListener("DOMContentLoaded", () => {
+loadContent();
 
 });
-const container = document.getElementById("container");
-for( let i=1;i <=5; i++)
-{
-    let el = document.createElement("div");
-    el.className ="item";
-    el.id = "card" + i;
-    el.innerHTML = `
-        <img src="src/img/${i}.jpg" alt="pizza" class="itemimg">
+const loadContent = () => {
+    const container = document.getElementById("container");
+    let id = 1;
+    for(const i of pizzaList)
+    {
+        let el = document.createElement("div");
+        el.className ="item";
+        el.id = "item" + id;
+        el.innerHTML = `
+        <h3>${i.name}</h3>
+        <img src="src/img/${i.name + id}.jpg" alt="pizza" class="itemimg">
         <div class="itemFooter">
-            <button>Buy</button>
+            <button>
+                <h3>Buy</h3>
+            </button>
             <button>
                 <img src="src/img/add+to+cart.png" alt="add to cart" class="itemFooterimg">
             </button>
-            </div>
+        </div>
     `;
-    container.append(el);
+        id++;
+        container.append(el);
+    }
 }
