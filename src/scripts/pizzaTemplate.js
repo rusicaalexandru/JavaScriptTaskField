d46fef6df4dfb1e579b028d2e8a13549c2c303d5
Rusica Alexantru <aalexandruslan@gmail.com>
2021-05-20 16:00:53 +0300

    REFACTOR
    once again changed code entierly
    (changes in progress)

---
+++ src/scripts/pizzaTemplate.js
@@ -0,0 +1,17 @@
+let PizzaObj = {
+    name: '',
+    // price: Number,
+    form: {
+        round: 10,
+        oval: 15
+    },
+    type: {
+        thin: 10,
+        thick: 15
+    },
+    size: {
+        small: 20,
+        med: 30,
+        big: 40
+    }
+}
