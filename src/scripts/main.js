794954f05ec89f95a04bedefde381ca0b82b1cd0
Rusica Alexantru <aalexandruslan@gmail.com>
2021-05-22 15:40:51 +0300

    backup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,17 +8,22 @@ let pizzaList = [
     {name:"abdul",price:60},
     {name:"monako",price:750}
 ];
+let tipBlat = [
+    {name:"gros",price:20},
+    {name:"subtire",price:15}
+];
 
 document.addEventListener("DOMContentLoaded", () => {
     loadContent();
-    document.getElementById("customeButton").addEventListener("click", () =>{
+    createCustomePizza();
+    /*document.getElementById("customeButton").addEventListener("click", () =>{ //temp
         createCustomePizza();
-    });
+    });*/
 })
 const loadContent = () => {
     const container = document.getElementById("container");
-    let id = 1;
-    for(const i of pizzaList)
+    let id = 1; //ussed to num the id of the elements
+    for(const i of pizzaList) //spawn elements from DB of pizzas
     {
         let el = document.createElement("div");
         el.className ="item";
@@ -30,7 +35,7 @@ const loadContent = () => {
             <button>
                 <h3>Buy</h3>
             </button>
-            <button>
+            <button id="cart${+id}">
                 <img src="src/img/add+to+cart.png" alt="add to cart" class="itemFooterimg">
             </button>
         </div>
@@ -40,5 +45,21 @@ const loadContent = () => {
     }
 }
 const createCustomePizza = () => {
-    
+    const container = document.getElementById("pizzaForm");
+    let id = 1; //ussed to num the id of the elements
+    container.append(document.createElement("ul"));
+    for(const i of tipBlat) //spawn elements from DB of pizzas
+    {
+        let el = document.createElement("li");
+        //el.className ="item";
+        el.id = "item" + id;
+        el.innerHTML =`hi`;
+
+        id++;
+        container.append(el);
+    }
+
+}
+const moveToCart = (item) => {
+
 }
